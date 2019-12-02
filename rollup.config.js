2eb7b152a9098b7b266ada4d1d921e1e7f7ec754
Rick Calder <rick@calder.io>
2019-12-02 11:27:44 -0500

    fix issues with punycode

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
 import json from 'rollup-plugin-json';
+import builtins from 'rollup-plugin-node-builtins';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
@@ -20,25 +21,12 @@ export default {
 		input: config.client.input(),
 		output: config.client.output(),
 		plugins: [
+			builtins(),
 			json({
-				// All JSON files will be parsed by default,
-				// but you can also specifically include/exclude files
 				include: 'node_modules/**',
 				exclude: [ 'node_modules/foo/**', 'node_modules/bar/**' ],
-	
-				// for tree-shaking, properties will be declared as
-				// variables, using either `var` or `const`
-				preferConst: true, // Default: false
-	
-				// specify indentation for the generated default export —
-				// defaults to '\t'
+				preferConst: true, 
 				indent: '  ',
-	
-				// ignores indent and generates the smallest code
-				compact: true, // Default: false
-	
-				// generate a named export for every property of the JSON object
-				namedExports: true // Default: true
 			}),
 			replace({
 				'process.browser': true,
@@ -53,7 +41,11 @@ export default {
 				browser: true,
 				dedupe
 			}),
-			commonjs(),
+			commonjs({
+        namedExports: {
+            'resource-loader': ['Resource']
+        }
+    }),
 
 			legacy && babel({
 				extensions: ['.js', '.mjs', '.html', '.svelte'],
